eb40f06a04ee8b6e688465bc27e0a797ed1cbdd9
Nathan Miller <natem31190@gmail.com>
2020-12-17 12:18:04 -0500

    commit tree 4ed274766a654355de4dbcf7e9a22843c65e9b2e

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  def square_array(array)
+    new_array = []
+    array.each do |num|
+      new_array << (num**2)
 end
+  return new_array
+ end 
+end
+
+
+
